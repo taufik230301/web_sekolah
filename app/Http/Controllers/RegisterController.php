a96761dbca88816c536a9e6949a05fd6d2fe5e73
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-04-08 09:26:25 +0700

    add register admin

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,6 +13,11 @@ class RegisterController extends Controller
     return view('register');
    }
 
+   public function register_view_admin()
+   {
+    return view('register_admin');
+   }
+
    public function store(Request $request)
    {
 
@@ -54,22 +59,22 @@ class RegisterController extends Controller
     //    dd('submitted');
     // // 
     
-    $nomor_pendaftaran = $request->nomor_pendaftaran;
-    $nidn = $request->nidn;
-    $tanggal_lahir = $request->tanggal_lahir;
+    $username = $request->username;
+    $password = $request->password;
+    $no_telp = $request->no_telp;
     $email = $request->email;
-    $id = sha1($nomor_pendaftaran.$nidn);
+    $id = sha1($username.$password);
     
     
         try {
-            DB::transaction(function () use ($id, $email, $nomor_pendaftaran, $nidn) {
-                DB::insert("INSERT INTO user(id,email,id_user_level,id_user_detail) VALUES('$id','$email','2','$id')");
-                DB::insert("INSERT INTO user_detail(id_user_detail,nomor_pendaftaran,nidn) VALUES('$id','$nomor_pendaftaran','$nidn')");
+            DB::transaction(function () use ($id, $email, $username, $password, $no_telp) {
+                DB::insert("INSERT INTO user(id,email,username,password,no_telp,id_user_level,id_user_detail) VALUES('$id','$email','$username','$password','$no_telp','1','$id')");
+                DB::insert("INSERT INTO user_detail(id_user_detail) VALUES('$id')");
             });
                 return redirect()
                     ->route('login_web')
                     ->with([
-                        'success' => 'Anda Telah Terdaftar !'
+                        'success' => 'Anda Telah Terdaftar Sebagai Admin !'
                     ]);
             } catch (\Exception $e) {
                 return redirect()
