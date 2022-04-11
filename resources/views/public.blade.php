bcc2cea7df5bf0b8c03d292e87ca537c6dd5fa7a
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-04-11 12:08:29 +0700

    add gambar home

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -91,15 +91,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                    <h1 data-aos="fade-up">Grow your business with Vesperr</h1>
-                    <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with
-                        Bootstrap</h2>
+                    <h1 data-aos="fade-up">Website Pendaftaran Ulang SMA Negeri 1 Tigapanah</h1>
+                    <h2 data-aos="fade-up" data-aos-delay="400">Selamat Datang !</h2>
                     <div data-aos="fade-up" data-aos-delay="800">
                         <a href="#about" class="btn-get-started scrollto">Get Started</a>
                     </div>
                 </div>
                 <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
-                    <img src="{{ asset('public/assets/img/hero-img.png') }}" class="img-fluid animated" alt="">
+                    <img src="{{ asset('public/assets/img/home.jpg') }}" class="img-fluid animated" alt="">
                 </div>
             </div>
         </div>
@@ -107,46 +106,6 @@
     </section><!-- End Hero -->
 
     <main id="main">
-
-        <!-- ======= Clients Section ======= -->
-        <section id="clients" class="clients clients">
-            <div class="container">
-
-                <div class="row">
-
-                    <div class="col-lg-2 col-md-4 col-6">
-                        <img src="{{ asset('public/assets/img/clients/client-1.png') }}" class="img-fluid" alt="" data-aos="zoom-in">
-                    </div>
-
-                    <div class="col-lg-2 col-md-4 col-6">
-                        <img src="{{ asset('public/assets/img/clients/client-2.png') }}" class="img-fluid" alt="" data-aos="zoom-in"
-                            data-aos-delay="100">
-                    </div>
-
-                    <div class="col-lg-2 col-md-4 col-6">
-                        <img src="{{ asset('public/assets/img/clients/client-3.png') }}" class="img-fluid" alt="" data-aos="zoom-in"
-                            data-aos-delay="200">
-                    </div>
-
-                    <div class="col-lg-2 col-md-4 col-6">
-                        <img src="{{ asset('public/assets/img/clients/client-4.png') }}" class="img-fluid" alt="" data-aos="zoom-in"
-                            data-aos-delay="300">
-                    </div>
-
-                    <div class="col-lg-2 col-md-4 col-6">
-                        <img src="{{ asset('public/assets/img/clients/client-5.png') }}" class="img-fluid" alt="" data-aos="zoom-in"
-                            data-aos-delay="400">
-                    </div>
-
-                    <div class="col-lg-2 col-md-4 col-6">
-                        <img src="{{ asset('public/assets/img/clients/client-6.png') }}" class="img-fluid" alt="" data-aos="zoom-in"
-                            data-aos-delay="500">
-                    </div>
-
-                </div>
-
-            </div>
-        </section><!-- End Clients Section -->
 
         <!-- ======= About Us Section ======= -->
         <section id="about" class="about">
